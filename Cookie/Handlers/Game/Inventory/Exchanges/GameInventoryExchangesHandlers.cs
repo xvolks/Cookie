f15378733cc9c71601249f70da5dae3f2e55538d
Christopher Yovanovitch <yovano_c@outlook.com>
2017-06-21 15:25:33 +0200

    Update MaxWeight on exchange

--- Cookie/Handlers/Game/Inventory/Exchanges/GameInventoryExchangesHandlers.cs
+++ Cookie/Handlers/Game/Inventory/Exchanges/GameInventoryExchangesHandlers.cs
@@ -28,18 +28,19 @@ namespace Cookie.Handlers.Game.Inventory.Exchanges
         private void ExchangeLeaveMessageHandler(DofusClient client, ExchangeLeaveMessage message)
         {
             if (!message.Success)
-            client.Logger.Log($"Echange annulé.", LogMessageType.Info);
+            client.Logger.Log("Echange annulé.", LogMessageType.Info);
         }
         [MessageHandler(ExchangeErrorMessage.ProtocolId)]
         private void ExchangeErrorMessageHandler(DofusClient client, ExchangeErrorMessage message)
         {         
-            client.Logger.Log($"Un erreur est survenue lors de l'échange.", LogMessageType.Info);
+            client.Logger.Log("Un erreur est survenue lors de l'échange.", LogMessageType.Info);
         }
         [MessageHandler(ExchangeStartedWithPodsMessage.ProtocolId)]
         private void ExchangeStartedWithPodsMessageHandler(DofusClient client, ExchangeStartedWithPodsMessage message)
         {
             client.Account.Character.Weight = message.FirstCharacterCurrentWeight;
-            client.Logger.Log($"Vous avez accepté l'échange.", LogMessageType.Info);
+            client.Account.Character.MaxWeight = message.FirstCharacterMaxWeight;
+            client.Logger.Log("Vous avez accepté l'échange.", LogMessageType.Info);
             client.Logger.Log($"Vous avez {message.FirstCharacterCurrentWeight} / {message.FirstCharacterMaxWeight} pods", LogMessageType.Info);
             client.Logger.Log($"L'échangeur a {message.SecondCharacterCurrentWeight} / {message.SecondCharacterMaxWeight} pods", LogMessageType.Info);
         }
