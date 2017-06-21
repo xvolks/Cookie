d74b72abc268d4bbe42255b31e5f0c7b057521a6
SapientiaFr <airstyle59@gmail.com>
2017-06-21 19:59:49 +0200

    Add Commande : MapID

---
+++ Cookie/Commands/Commands/MapIdCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Cookie.Commands.Interfaces;
+using Cookie.Core;
+using Cookie.Utils.Enums;
+
+namespace Cookie.Commands.Commands
+{
+    public class MapIdCommand : ICommand
+    {
+        public string CommandName => "mapid";
+
+        public void OnCommand(DofusClient client, string[] args)
+        {
+                client.Logger.Log("MapID : " + client.Account.Character.MapId,
+                    LogMessageType.Admin);           
+        }
+    }
+}
