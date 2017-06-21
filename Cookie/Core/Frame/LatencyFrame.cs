2133e2e0ff84fd58fdd98933c396ef6026444ece
Christopher Yovanovitch <yovano_c@outlook.com>
2017-06-21 15:41:43 +0200

    Fix Sequence

--- Cookie/Core/Frame/LatencyFrame.cs
+++ Cookie/Core/Frame/LatencyFrame.cs
@@ -19,6 +19,8 @@ namespace Cookie.Core.Frame
         {
             _latencyBuffer = new List<uint>();
             _account = account;
+
+            Sequence = 0;
         }
 
         public void LowSend()
