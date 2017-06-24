48815af4f866c3d4b4fe7682e1e3f446959142c3
Karrazyne <karradev30@gmail.com>
2017-06-25 00:29:25 +0200

    Update API

--- Cookie.API/Core/IDofusClient.cs
+++ Cookie.API/Core/IDofusClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Cookie.API.Protocol;
 using Cookie.Core;
 
 namespace Cookie.API.Core
@@ -6,6 +7,7 @@ namespace Cookie.API.Core
     public interface IDofusClient
     {
         void Register(Type type);
+        void Send(NetworkMessage message);
         void Log(string text, LogMessageType type = LogMessageType.Default);
     }
 }
