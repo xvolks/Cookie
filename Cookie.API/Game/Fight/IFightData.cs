84a2778c207929665f04b1ba998a80a466318f4b
Aursen <jeanno11@orange.fr>
2017-07-04 01:35:06 +0200

    Beginning of fight functions

---
+++ Cookie.API/Game/Fight/IFightData.cs
@@ -0,0 +1,33 @@
+﻿using Cookie.API.Game.Fight.Fighters;
+using System.Collections.Generic;
+
+namespace Cookie.API.Game.Fight
+{
+    public interface IFightData
+    {
+        /// <summary>
+        ///     Liste des combattants
+        /// </summary>
+        List<IFighter> Fighters { get; }
+        /// <summary>
+        ///     Entité du bot
+        /// </summary>
+        IFighter Fighter { get; }
+        /// <summary>
+        ///     Id du fighter au moment du tour
+        /// </summary>
+        int TurnId { get; }
+        /// <summary>
+        ///     Récupère les cellules disponibles
+        /// </summary>
+        List<int> GetReachableCells();
+        /// <summary>
+        ///     Vérifie si le bot est main à main avec quelqu'un
+        /// </summary>
+        bool IsHandToHand();
+        /// <summary>
+        ///     Retourne le monstre le plus proche
+        /// </summary>
+        IMonster NearestMonster();
+    }
+}
---
+++ Cookie/Game/Fight/Fighters/Fighter.cs
@@ -0,0 +1,27 @@
+﻿using Cookie.API.Game.Fight.Fighters;
+using Cookie.API.Protocol.Network.Types.Game.Context.Fight;
+
+namespace Cookie.Game.Fight.Fighters
+{
+    public class Fighter : Entity.Entity, IFighter
+    {
+        public Fighter(int id, int cellId, GameFightMinimalStats stats, uint teamId, bool isAlive) : base(id, cellId)
+        {
+            Stats = stats;
+            TeamId = teamId;
+            IsAlive = isAlive;
+            LifePoints = stats.LifePoints;
+            MaxLifePoints = stats.MaxLifePoints;
+            ActionPoints = stats.ActionPoints;
+            MovementPoints = stats.MovementPoints;
+        }
+
+        public uint LifePoints { get; internal set; }
+        public uint MaxLifePoints { get; internal set; }
+        public short ActionPoints { get; internal set; }
+        public short MovementPoints { get; internal set; }
+        public bool IsAlive { get; internal set; }
+        public uint TeamId { get; internal set; }
+        public GameFightMinimalStats Stats { get; internal set; }
+    }
+}
