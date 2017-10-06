67768021edd0510791731893a0cc1653b8948857
Karrazyne <karradev30@gmail.com>
2017-10-06 16:56:27 +0200

    Update version

--- Cookie.API/Utils/GameConstant.cs
+++ Cookie.API/Utils/GameConstant.cs
@@ -6,8 +6,8 @@ namespace Cookie.API.Utils
     {
         public const byte Major = 2;
         public const byte Minor = 43;
-        public const byte Release = 5;
-        public const int Revision = 0;
+        public const byte Release = 6;
+        public const int Revision = 1;
         public const byte Patch = 0;
         public const byte BuildType = (byte) BuildTypeEnum.RELEASE;
         public const byte Install = (byte) ClientInstallTypeEnum.CLIENT_BUNDLE;
