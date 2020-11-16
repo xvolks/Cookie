a35ab194c6b42a38a1c1d95b15bd0ac041857674
Teles <mteleshenrique@gmail.com>
2020-11-16 16:53:52 -0500

    D2oReader Added.

---
+++ D2oReader/D2oReader/Dofus/DofusEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace D2oReader
+{
+        public enum GameDataTypeEnum
+        {
+            Int = -1,
+            Bool = -2,
+            String = -3,
+            Double = -4,
+            I18N = -5,
+            UInt = -6,
+            Vector = -99,
+        };
+}
---
+++ D2oReader/D2oReader/Dofus/GameDataClassDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace D2oReader
+{
+    public class GameDataClassDefinition
+    {
+        public string Name;
+        public string PackageName;
+        public List<GameDataField> Fields;
+
+        public GameDataClassDefinition(string packageName,string className)
+        {
+            Fields = new List<GameDataField>();
+            Name = className;
+            PackageName = packageName;
+        }
+
+        internal void AddField(D2OReader reader)
+        {
+            GameDataField field = new GameDataField(reader);
+
+            Fields.Add(field);
+        }
+    }
+}
